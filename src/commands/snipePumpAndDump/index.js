76b7adadbb4c56deb48739818054ae0da37525dd
rsytkiv <rsytkiv@gmail.com>
2024-11-12 01:55:05 +0200

    Fixed pumps and dumps sniping

--- src/commands/snipePumpAndDump/index.js
+++ src/commands/snipePumpAndDump/index.js
@@ -10,56 +10,60 @@ const priceHistory = {};
 const sentOpportunities = new Set();
 
 export const snipePumpAndDump = async (bot, chatId , userPumpsAndDumpsSettings) => {
-    try {
-        const { exchanges, percentage } = userPumpsAndDumpsSettings;
-        let currentPrices = [];
+    const { exchanges, percentage } = userPumpsAndDumpsSettings;
 
-        console.log('working...');
-        sendMessageWithHTML(bot, chatId, `<b>Sniping started with the following options:</b>\n\n<b>Exchanges:</b> ${exchanges.join(', ')}\n<b>Percentage:</b> ${percentage}%`);
+    sendMessageWithHTML(bot, chatId, `<b>Sniping started with the following options:</b>\n\n<b>Exchanges:</b> ${exchanges.join(', ')}\n<b>Percentage:</b> ${percentage}%`);
 
-        currentPrices = await fetchDataFromChosenExchanges(exchanges, currentPrices, bot, chatId);
+    setInterval(async () => {
+        try {
+            let currentPrices = [];
 
-        const now = Date.now();
-        const arbitrageOpportunities = [];
+            console.log('working...');
 
-        currentPrices?.forEach(({ symbol, price, exchange, volume }) => {
-            if (!priceHistory[symbol]) {
-                priceHistory[symbol] = [];
-            }
+            currentPrices = await fetchDataFromChosenExchanges([...exchanges, 'mexc'], currentPrices, bot, chatId);
 
-            priceHistory[symbol].push({ time: now, price });
+            const now = Date.now();
+            const arbitrageOpportunities = [];
 
-            // Remove old prices outside the time window
-            priceHistory[symbol] = priceHistory[symbol].filter(p => now - p.time <= TIME_WINDOW);
+            currentPrices?.forEach(({ symbol, price, exchange, volume }) => {
+                if (!priceHistory[symbol]) {
+                    priceHistory[symbol] = [];
+                }
 
-            // Check for price changes within the time window
-            const oldestPrice = priceHistory[symbol][0].price;
-            const priceChange = ((price - oldestPrice) / oldestPrice) * 100;
+                priceHistory[symbol].push({ time: now, price });
 
-            // Check for price pumps (positive change) and dumps (negative change)
-            if (Math.abs(priceChange) >= percentage) {
-                const direction = priceChange > 0 ? 'pump' : 'dump';
-                const opportunity = `<code>${symbol}</code>: ${priceChange.toFixed(2)}% ${direction} (Old: ${oldestPrice} ➡️ New: ${price})\n<b>Volume:</b> ${volume} \n<b>Exchange:</b> ${exchange}`;
+                // Remove old prices outside the time window
+                priceHistory[symbol] = priceHistory[symbol].filter(p => now - p.time <= TIME_WINDOW);
 
-                // Check if this opportunity has already been sent
-                if (!sentOpportunities.has(symbol)) {
-                    sentOpportunities.add(symbol); // Mark this opportunity as sent
-                    arbitrageOpportunities.push(opportunity);
+                // Check for price changes within the time window
+                const oldestPrice = priceHistory[symbol][0].price;
+                const priceChange = ((price - oldestPrice) / oldestPrice) * 100;
+
+                // Check for price pumps (positive change) and dumps (negative change)
+                if (Math.abs(priceChange) >= percentage) {
+                    const direction = priceChange > 0 ? 'pump' : 'dump';
+                    const opportunity = `<code>${symbol}</code>: ${priceChange.toFixed(2)}% ${direction} (Old: ${oldestPrice} ➡️ New: ${price})\n<b>Volume:</b> ${volume} \n<b>Exchange:</b> ${exchange}`;
+
+                    // Check if this opportunity has already been sent
+                    if (!sentOpportunities.has(symbol)) {
+                        sentOpportunities.add(symbol); // Mark this opportunity as sent
+                        arbitrageOpportunities.push(opportunity);
+                    }
+                }
+            });
+
+            // Send alerts for arbitrage opportunities
+            if (arbitrageOpportunities.length > 0) {
+                for (let opportunity of arbitrageOpportunities) {
+                    sendMessageWithHTML(bot, chatId, `⚠️ <b>Price change detected:</b>\n\n${opportunity}`);
                 }
             }
-        });
+        } catch (error) {
+            console.error('Error in main sniper block:', error);
 
-        // Send alerts for arbitrage opportunities
-        if (arbitrageOpportunities.length > 0) {
-            for (let opportunity of arbitrageOpportunities) {
-                sendMessageWithHTML(bot, chatId, `⚠️ <b>Price change detected:</b>\n\n${opportunity}`);
-            }
+            logErrorToFile(error, chatId, bot);
         }
-    } catch (error) {
-        console.error('Error in main sniper block:', error);
-
-        logErrorToFile(error, chatId, bot);
-    }
+    }, 30000);
 };
 
 export default {
